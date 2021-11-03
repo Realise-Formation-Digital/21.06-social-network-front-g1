e909b9c4568cc4baf8ba87aa8235a79618e6f899
Dos Santos Davide <85991452+Superwinny@users.noreply.github.com>
2021-11-03 11:40:10 +0100

    commit
    modification footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,20 @@
     dark
     padless
     absolute
+    class="d-flex justify-center"
   >
     <v-card
+    
+      align="center"
       flat
       tile
-      class="indigo lighten-1 white--text text-center"
+      class="indigo lighten-1 white--text width-100"
     >
+    <v-card-text class="white--text pt-0">
+      Faites appel à nous pour peaufiner votre projet, du choix des hôtels à la sélection de vos vols, du dessin de votre itinéraire au tri des activités.  Chez nous, tout est possible !
+
+      </v-card-text>
+
       <v-card-text>
         <v-btn
           v-for="icon in icons"
@@ -22,10 +30,7 @@
         </v-btn>
       </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </v-card-text>
-
+      
       <v-divider></v-divider>
 
       <v-card-text class="white--text">
@@ -51,5 +56,8 @@ export default {
 </script>
 
 <style>
+  .width-100 {
+    width: 100%;
+  }
 
 </style>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Home</div>
+  <div>Contact</div>
 </template>
 
 <script>
