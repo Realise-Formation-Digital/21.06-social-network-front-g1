ae332225da4271da5cabbb742997e8a977e37247
IEDIN2021 <85932847+IEDIN2021@users.noreply.github.com>
2021-11-04 12:25:50 +0100

    page agence

--- src/pages/Agence.vue
+++ src/pages/Agence.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Agence</h1>
+<b-container class="hslika" fluid="xxl">
+  <b-row class="text-center">
+    <b-col></b-col>
+    <b-col class="textitre" cols="8">L'AGENCE
+      <p class="textp">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+
+    </b-col>
+    <b-col></b-col>
+  </b-row>
+</b-container>
 </template>
 
 
@@ -10,5 +19,18 @@
 <style scoped>
 .edinpadding {
   padding-top: 25px;
+}
+.hslika {
+  background-image: url("http://anjiusa.com/img/bg/background-subheader-1-about.jpg");
+  max-height: 300px;
+  height: 300px;
+}
+.textitre {
+  margin-top:170px;
+  color:white;
+  font-size:32px;
+}
+.textp {
+  font-size:16px;
 }
 </style>
