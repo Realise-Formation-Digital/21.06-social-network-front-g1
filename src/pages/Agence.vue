b1ac923fe593013cc04f270edf97a285966c0e61
IEDIN2021 <85932847+IEDIN2021@users.noreply.github.com>
2021-11-05 08:19:10 +0100

    Update Agence.vue

--- src/pages/Agence.vue
+++ src/pages/Agence.vue
@@ -1,14 +1,205 @@
 <template>
-<b-container class="hslika" fluid="xxl">
-  <b-row class="text-center">
-    <b-col></b-col>
-    <b-col class="textitre" cols="8">L'AGENCE
-      <p class="textp">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+  <div>
+    <!-- OUR PAGE TITLE!-->
+    <b-container class="hslika" fluid>
+      <b-row class="text-center">
+        <b-col></b-col>
+        <b-col class="textitre" cols="12">L'AGENCE </b-col>
+      </b-row>
+      <b-row class="text-center">
+        <b-col></b-col>
+        <b-col class="textp" cols="12"
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore
+        </b-col>
+      </b-row>
+    </b-container>
 
-    </b-col>
-    <b-col></b-col>
-  </b-row>
-</b-container>
+    <!-- CONTENT OF PAGE !-->
+    <b-container class="bv-example-row margintop80px">
+      <b-row>
+        <b-col md="6">
+          <b-card-body class="text-left">
+            <b-card-title class="text26px">You’re in good company</b-card-title>
+
+            <b-card-text>
+              <p class="fontwight800">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore
+              </p>
+            </b-card-text>
+          </b-card-body>
+
+          <b-container fluid class="p-4 bg-white">
+            <b-row>
+              <b-col sm="6" md="6" lg="6" xl="3" >
+                <b-img
+                  thumbnail
+                  fluid
+                  src="https://picsum.photos/250/250/?image=54"
+                  style="max-width: 100%"
+                  alt="Image 1"
+                ></b-img>
+              </b-col>
+              <b-col sm="6" md="6" lg="6" xl="3" >
+                <b-img
+                  thumbnail
+                  fluid
+                  src="https://picsum.photos/250/250/?image=58"
+                  alt="Image 2"
+                ></b-img>
+              </b-col>
+              <b-col sm="6" md="6" lg="6" xl="3" >
+                <b-img
+                  thumbnail
+                  fluid
+                  src="https://picsum.photos/250/250/?image=59"
+                  alt="Image 3"
+                ></b-img>
+              </b-col>
+            </b-row>
+            <b-row class="margintop40px">
+              <b-col sm="6" md="6" lg="6" xl="3" >
+                <b-img
+                  thumbnail
+                  fluid
+                  src="https://picsum.photos/250/250/?image=54"
+                  alt="Image 1"
+                ></b-img>
+              </b-col>
+              <b-col sm="6" md="6" lg="6" xl="3" >
+                <b-img
+                  thumbnail
+                  fluid
+                  src="https://picsum.photos/250/250/?image=58"
+                  alt="Image 2"
+                ></b-img>
+              </b-col>
+              <b-col sm="6" md="6" lg="6" xl="3" >
+                <b-img
+                  thumbnail
+                  fluid
+                  src="https://picsum.photos/250/250/?image=59"
+                  alt="Image 3"
+                ></b-img>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-col>
+        <!-- logo + desc of agency !-->
+        <b-col>
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 100%"
+            class="mb-2"
+          >
+            <b-card-text class="edin">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container >
+    <!-- OUR 4 CITY !-->
+    <b-container fluid class="margintop40px padding70 bg-dark">
+    <b-container >
+      <b-row class="text-center">
+        <b-col sm="6" md="6" lg="6" xl="3"  >
+          <b-card
+            title="Sarajevo"
+            img-src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Sarajevo.jpg/280px-Sarajevo.jpg"
+            img-alt="Image"
+            img-height="200px"
+            img-top
+            tag="article"
+            class="mb-2"
+          >
+            <b-card-text>
+              Sarajevo (/sa.ʁa.je.vo/ ; en bosnien cyrillique, Сарајево,
+              /sǎrajeʋo/ ; anciennement Bosna-Seraï ; en turc : Saraybosna) est
+              la capitale et la plus grande ville de Bosnie-Herzégovine.
+              Traversée par la rivière Miljacka, la ville fait partie du canton
+              de Sarajevo, l'un des dix de Bosnie-et-Herzégovine...
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col sm="6" md="6" lg="6" xl="3">
+          <b-card
+            title="New-york"
+            img-src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Lower_Manhattan_skyline_-_June_2017.jpg/279px-Lower_Manhattan_skyline_-_June_2017.jpg"
+            img-alt="Image"
+            img-height="200px"
+            img-top
+            tag="article"
+            class="mb-2"
+          >
+            <b-card-text>
+              New Yorka (prononcé en anglais : /nu ˈjɔɹk/b Écouter),
+              officiellement nommée City of New York, connue également sous les
+              noms et abréviations de New York City ou NYC (pour éviter la
+              confusion avec l'État de New York), et dont le surnom le plus
+              connu est The Big Apple...
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col sm="6" md="6" lg="6" xl="3">
+          <b-card
+            title="Bern"
+            img-src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/City_of_Berne.jpg/280px-City_of_Berne.jpg"
+            img-alt="Image"
+            img-top
+            tag="article"
+            img-height="200px"
+            class="mb-2"
+          >
+            <b-card-text>
+              Berne (en allemand : Bern, en italien et en romanche : Berna
+              Écouter) est la capitale de facto de la Suisse, le chef-lieu du
+              canton homonyme et la cinquième3 plus grande ville de Suisse.
+              Depuis 1848, Berne est la « ville fédérale »4, à savoir le siège
+              permanent du gouvernement fédéral...
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col sm="6" md="6" lg="6" xl="3">
+          <b-card
+            title="Paris"
+            img-src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/278px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
+            img-alt="Image"
+            img-top
+            tag="article"
+            img-height="200px"
+            class="mb-2"
+          >
+            <b-card-text>
+              Elle se situe au cœur d'un vaste bassin sédimentaire aux sols
+              fertiles et au climat tempéré, le bassin parisien, sur une boucle
+              de la Seine, entre les confluents de celle-ci avec la Marne et
+              l'Oise. Paris est également le chef-lieu de la région
+              Île-de-France et le centre de la métropole du Grand Paris....
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+    </b-container>
+  </div>
 </template>
 
 
@@ -16,7 +207,7 @@
 </script>
 
 
-<style scoped>
+<style>
 .edinpadding {
   padding-top: 25px;
 }
@@ -26,11 +217,36 @@
   height: 300px;
 }
 .textitre {
-  margin-top:170px;
-  color:white;
-  font-size:32px;
+  margin-top: 100px !important;
+  color: white;
+  font-size: 32px;
 }
 .textp {
-  font-size:16px;
+  font-size: 16px;
+  color: white;
+}
+.margintop80px {
+  margin-top: 80px;
+}
+.margintop60px {
+  margin-top: 60px;
+}
+.margintop40px {
+  margin-top: 40px;
+}
+.margintop20px {
+  margin-top: 20px;
+}
+.text26px {
+  font-size: 46px;
+  font-weight: 800;
+  margin-bottom: 50px;
+}
+.fontwight800 {
+  font-size: 18px;
+  font-weight: 800;
+}
+.padding70 {
+  padding: 70px;
 }
 </style>
