8139aff08d379aabcc8c04844db831c593557cdb
IEDIN2021 <85932847+IEDIN2021@users.noreply.github.com>
2021-11-09 15:35:36 +0100

    contac

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -13,158 +13,111 @@
         </b-col>
       </b-row>
     </b-container>
+    <b-container fluid>
+      <b-container>
+        <b-container class="bv-example-row padding80">
+          <b-row>
+            <b-col>
+              <b-card-body title="ADRESSE">
+                <b-card-text>
+                  Quartier Monplaisir 174, <br />avenue des Frères Lumière -
+                  69008 LYON
+                </b-card-text>
+              </b-card-body>
+              <b-row>
+                <b-container class="bv-example-row padding80">
+                  <b-row>
+                    <b-col>
+                      <b-card-body>
+                        <b-card-text>
+                          <button type="button" class="btn btn-primary">
+                            022 422 33 66
+                          </button>
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                    <b-col>
+                      <b-card-body>
+                        <b-card-text>
+                          <button type="button" class="btn btn-primary">
+                            022 422 33 66
+                          </button>
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-container></b-row
+              >
+            </b-col>
 
-    <b-container>
-      <b-overlay mode="out-in">
-        <b-row>
-          <!--<bg-img class="grey darken-4" link href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fitourisme.net%2Fque-faire-a-manille%2F&psig=AOvVaw0bmW4KQx_BkfFfQBXhzTuh&ust=1636034916821000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCG5OThivQCFQAAAAAdAAAAABAD"
-          src="https://itourisme.net/wp-content/uploads/2016/09/top-10-things-to-do-in-manila.jpg"
-          height="125">
-         
-        
-          
-        </bg-img>!-->
-        </b-row>
-      </b-overlay>
-
-      <b-row>
-        <b-col cols="12" sm="6">
-          <div>
-            <label for="name">Nom:</label><br />
-            <input id="name" type="text" v-model="name" required />
-          </div>
-        </b-col>
-      </b-row>
-
-      <b-row>
-        <b-col cols="12" sm="6">
-            <div>
-              <label for="email">Email:</label><br />
-              <input id="email" type="text" v-model="email" required />
-            </div>
-
-            <b-row>
-              <b-col cols="12" sm="6">
+            <b-col>
+              <div class="contact">
+                <input
+                  id="name"
+                  type="text"
+                  v-model="name"
+                  placeholder="Nom"
+                  required
+                />
+                <input
+                  id="prenom"
+                  type="text"
+                  v-model="prenom"
+                  placeholder="Prénom"
+                  required
+                /><br />
+                <input
+                  id="email"
+                  type="text"
+                  v-model="email"
+                  placeholder="Adresse Email"
+                  required
+                /><br />
+                <input
+                  id="prenom"
+                  type="text"
+                  v-model="prenom"
+                  placeholder="Prénom"
+                  required
+                /><br />
+                <textarea
+                  placeholder="Message"
+                  id="message"
+                  type="text"
+                  rows="3"
+                  v-model="message"
+                  required
+                /><br /><br />
+                <b-button
+                  squared
+                  variant="primary"
+                  :class="[name ? activeClass : '']"
+                  @click="submitForm()"
+                  >Envoyer</b-button
+                >
                 <div>
-                  <label for="message">Message:</label><br />
-                  <input id="message" type="text" v-model="message" required />
+                  <h5>
+                    <p b-if="response">{{ response }}</p>
+                  </h5>
                 </div>
-              </b-col>
-            </b-row>
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
 
-            <br />
-            <b-button
-              squared
-              variant="primary"
-              :class="[name ? activeClass : '']"
-              @click="submitForm()"
-              >Envoyer</b-button
-            >
-            <div>
-              <h5>
-                <p b-if="response">{{ response }}</p>
-              </h5>
-            </div>
-
-          <!-- Maps -->
-          <div id="map">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2761.9127650590526!2d6.200637315876729!3d46.19229459258493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x21a409b191840de1%3A0x55206748a6794bc4!2sCr%C3%A9ation%20de%20site%20internet%20%C3%A0%20Gen%C3%A8ve%20-%20Creasite.ch!5e0!3m2!1sfr!2sch!4v1634803456049!5m2!1sfr!2sch"
-              width="100%"
-              height="480"
-              style="border: 0"
-              allowfullscreen=""
-              loading="lazy"
-            ></iframe>
-          </div>
-        </b-col>
-      </b-row>
-
+        <!-- Maps -->
+        <b-container fluid>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2761.877517634499!2d6.126591315581985!3d46.192995979116255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478c7b2e1299827b%3A0xb6bbc30dd8e5306f!2zUsOpYWxpc2U!5e0!3m2!1sfr!2sch!4v1636465081519!5m2!1sfr!2sch"
+            width="100%"
+            height="450"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe>
+        </b-container>
+      </b-container>
       <!--titre la team-->
-      <b-row>
-        <b-col class="section-title-col" data-type="column">
-          <h2><span class="bold">L'équipe</span></h2>
-          <br />
-          <p class="lead">
-            Nos developpeurs vous accueillent et assistent au 8, rue Viguet à
-            Genève
-          </p>
-        </b-col>
-      </b-row>
-
-      <b-overlay mode="out-in">
-        <b-row><overflow-y>
-          <b-col cols="3">
-            <b-card>
-              <b-img
-                src="https://media-exp1.licdn.com/dms/image/C4D03AQGNkLl8KxsTvA/profile-displayphoto-shrink_200_200/0/1627053755082?e=1641427200&v=beta&t=klVkd87zX184bES5aV2_dwRVnp9zdP1p4dL3CLtm6CE"
-                height="300"
-                class="grey darken-4"
-              >
-              </b-img>
-              <b-card-text class="text-h6"
-                >Edin Ibrahimovic<br/>Grad Sarajevo</b-card-text
-              >
-            </b-card>
-          </b-col>
-
-          <b-col cols="3">
-            <b-card>
-              <b-img
-                src="https://media-exp1.licdn.com/dms/image/D4E35AQE30JKSkiP3LQ/profile-framedphoto-shrink_200_200/0/1633090403482?e=1636041600&v=beta&t=wGvTfFPZu2ImRGJHx70XTQeWCywhQpE5tm2GlwRaI9M"
-                height="300"
-                class="grey darken-4"
-              >
-              </b-img>
-              <b-card-text class="text-h6"
-                >Lionel<br />Ville de Dijon</b-card-text
-              >
-            </b-card>
-          </b-col>
-
-          <b-col cols="3">
-            <b-card>
-              <b-img
-                src="https://media-exp1.licdn.com/dms/image/C4D03AQGkkuXQxzfdXw/profile-displayphoto-shrink_100_100/0/1634142789940?e=1641427200&v=beta&t=vZGMSkkyAvdLs6WnhTIVL06vkqp7nfLMAt_N_aZnW_I"
-                height="300"
-                class="grey darken-4"
-              >
-              </b-img>
-              <b-card-text class="text-h6">Maverick<br />Manille</b-card-text>
-            </b-card>
-          </b-col>
-
-          <b-col cols="3">
-            <b-card>
-              <b-img
-                src="https://media-exp1.licdn.com/dms/image/C4E35AQG4scbQrOW1ww/profile-framedphoto-shrink_200_200/0/1617719919996?e=1636041600&v=beta&t=JJetLguSOUm0RY8aHdyK8WfnAptUa1tadEShkSQ8flo"
-                height="300"
-                class="grey darken-4"
-              >
-              </b-img>
-              <b-card-text class="text-h6"
-                >Davide<br />Tomar cidade</b-card-text
-              >
-            </b-card>
-          </b-col></overflow-y>
-        </b-row>
-      </b-overlay>
-
-      <b-overlay mode="out-in">
-        <b-row>
-          <b-col cols="3">
-            <b-card>
-              <b-img
-                src="https://media-exp1.licdn.com/dms/image/C4D03AQFWEghYu0IZJA/profile-displayphoto-shrink_200_200/0/1517401072068?e=1641427200&v=beta&t=iwwxbCNWrJZJoJlNoEHo9hpptKYm-_HHzbNur1b7R2M"
-                height="300"
-                class="grey darken-4"
-              >
-              </b-img>
-              <b-card-text class="text-h6">Khady<br />Deuk Dakar</b-card-text>
-            </b-card>
-          </b-col>
-        </b-row>
-      </b-overlay>
     </b-container>
   </div>
 </template>
@@ -221,5 +174,14 @@ export default {
 
 .text-h6 {
   text-align: center;
+}
+.padding80 {
+  padding: 80px;
+}
+#name {
+  width: 90%;
+}
+#email {
+  width: 100%;
 }
 </style>
